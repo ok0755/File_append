432fb755b4411edf6218e508ccebbf97f31edf13
ok0755 <37565231+ok0755@users.noreply.github.com>
2018-07-31 18:24:33 +0800

    Add files via upload

--- File_append.py
+++ File_append.py
@@ -1,7 +1,9 @@
 #coding=utf-8
+#author:Zhoubin
+
 import os
 import re
-#from multiprocessing import Pool, freeze_support
+from multiprocessing import Pool,freeze_support
 import pandas as pd
 
 #get not PDF files as list
@@ -12,8 +14,9 @@ def get_file(dirpath):
         if os.path.splitext(fil)[1] != '.pdf':
             br.append(fil)
     return br
+
 #write to xls
-def openxls(rootdir, xlsworkbook):
+def openxls(model, rootdir, xlsworkbook):
     print 'appending >>> ', xlsworkbook
     r = re.compile(r'^\d')
     path = os.path.join(rootdir, xlsworkbook)
@@ -30,28 +33,22 @@ def openxls(rootdir, xlsworkbook):
             data[k].drop([2], axis=1, inplace=True)
             data[k][9] = data[k][9].str.replace(' ', '')
             data[k][9] = data[k][9].str.replace('\n', '')
-            data[k].to_csv('t.csv', mode='a', header=None, index=None, encoding='gb18030')
+            data[k].to_csv('d:\\{}.csv'.format(model), mode='a', header=None, index=None, encoding='gb18030')
 
 if __name__ == '__main__':
-    #freeze_support()
+    freeze_support()
     model = raw_input('Enter model series:>> ')
     rootdir = u'J:\\PIE Process Manual\\新工序文件(CMP)\\工序手冊\\{}\\'.format(model)
     ar = get_file(rootdir)
-    #p = Pool(4)
+    p = Pool(4)
     for a in ar:
-        openxls(rootdir,a)
-        #p.apply(openxls, args=(rootdir, a, ))
-    #p.close()
-    #p.join()
-    '''
-    p=Pool(4)
-    for a in ar:
-        p.apply(openxls,args=(files,a,))
+        p.apply(openxls, args=(model, rootdir, a, ))
     p.close()
     p.join()
-    '''
+    print 'D:\\{}.csv has been builded\n Copyright by Zhoubin'.format(model)
+    os.system('pause.exe')
 
-'''
+    '''
             d=data[k].iloc[4:-1,:]
             d.insert(0,'motor_model',xlsworkbook[:-4])
             d[0].fillna(method='pad',inplace=True)
@@ -68,4 +65,4 @@ if __name__ == '__main__':
         d=v[(v[0].str.startswith('A')) | (v[0].str.startswith('B')) | (v[0].str.startswith('C')) | (v[0].str.startswith('D')) | (v[0].str.startswith('E'))]
         d.to_csv('t.csv',mode='a',header=None,index=None,encoding='gb18030')
         #d=v[(v[1].str.contains(u'绕线',na=False)) | (v[1].str.contains(u'繞線',na=False))]
-'''
+    '''
